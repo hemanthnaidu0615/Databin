efe5a4101ce9e2e63e7e160afbb8a3900d5ab438
aminah <aminah.sultan@meridianit.com>
2024-07-25 17:56:18 +0530

    Bar chart Amount value change from k to M

--- DataBinFE/src/components/charts/BarChart.tsx
+++ DataBinFE/src/components/charts/BarChart.tsx
@@ -8,7 +8,7 @@ export const BarChart = ({ chartData }: any) => {
     if (value >= 1_000_000) {
       return `$${(value / 1_000_000).toFixed(1)}M`;
     } else if (value >= 1_000) {
-      return `$${(value / 1_000).toFixed(1)}k`;
+      return `$${(value / 1_000_000).toFixed(1)}M`;
     }
     return `$${value.toLocaleString()}`;
   };
