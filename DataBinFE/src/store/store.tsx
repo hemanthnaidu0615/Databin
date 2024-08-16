9321c9844b9dec461484854ee895f51a00516f36
hemanthnaidu0615 <hemanth.naidu@meridianit.com>
2024-08-16 10:54:43 +0530

    removed unused files

--- DataBinFE/src/store/store.tsx
+++ DataBinFE/src/store/store.tsx
@@ -3,14 +3,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import dateRangeReducer from "./dateRangeSlice";
 import enterpriseReducer from "./enterpriseSlice";
-import singleDateReducer from "./singleDateSlice"; // Import the new reducer
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     dateRange: dateRangeReducer,
     enterprise: enterpriseReducer,
-    singleDate: singleDateReducer, // Add the new reducer to the store
   },
 });
 
